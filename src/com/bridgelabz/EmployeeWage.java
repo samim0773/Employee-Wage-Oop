195e789d4d8e1dd3ea9cf43b3649a8a929ba8dd0
Samim Aktar <samimaktar0773@gmail.com>
2021-10-11 18:02:18 +0530

    Done UC1 Emp is present or absent

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,6 +3,10 @@ package com.bridgelabz;
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		CheckEmployeePresentAbsent empCheck = new CheckEmployeePresentAbsent();
+		empCheck.getIsEmpPresent();
+		
 	}
 
 }
